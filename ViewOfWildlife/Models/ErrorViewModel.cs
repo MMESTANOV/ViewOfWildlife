393364ea362cbb8d766607d6567d610468b201b5
MMESTANOV <mm_stan@abv.bg>
2024-11-11 21:46:27 +0200

    Add project files.

---
+++ ViewOfWildlife/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ViewOfWildlife.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
