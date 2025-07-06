748202242f4fe6efdbf555026d06f0cd67e854f4
MMESTANOV <mm_stan@abv.bg>
2025-07-06 14:55:07 +0300

    correct ServiceCollecrionExtension

--- ViewOfWildlife/Extensions/ServiceCollectionExtension.cs
+++ ViewOfWildlife/Extensions/ServiceCollectionExtension.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Extensions.DependencyInjection
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ViewOfWildlife.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtension
     {
@@ -9,11 +13,28 @@
 
         public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
         {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter(); 
+
             return services;
         }
 
         public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
         {
+            services
+             .AddDefaultIdentity<IdentityUser>(options =>
+             {
+                 options.SignIn.RequireConfirmedAccount = false;
+                 options.Password.RequireNonAlphanumeric = false;
+                 options.Password.RequireDigit = false;
+                 options.Password.RequireLowercase = false;
+                 options.Password.RequireUppercase = false;
+             })
+             .AddEntityFrameworkStores<ApplicationDbContext>();
+
             return services;
         }
     }
